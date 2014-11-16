1dbecb18af7422591c48f1ed9e43b908bf5cba67
Shallav <shallav@seas.upenn.edu>
2014-11-15 19:39:25 -0500

    Updated CrossValidation class. Included ROC helper provision.

--- CrossValidation.py
+++ CrossValidation.py
@@ -14,6 +14,14 @@ from sklearn.metrics import accuracy_score
 
 class CrossValidation:
 
+    def __init__(self, X, Y, cv = 10):
+        self.folds = cv
+        X = self.normalize(X)
+        allData = np.c_[X, Y]
+        self.folds = cv
+        ratio = 1.0 / cv
+        self.generateValTrainSets(allData, ratio)
+
     def splitDataLabelWise(self, allData):
         temp = allData[allData[:,-1].argsort()]
         pos = np.where(temp[:, -1] == 1)[0][0]
@@ -57,38 +65,46 @@ class CrossValidation:
                 indexPos.append((firstPos, lastPos))
             firstPos = firstPos + numPos
             firstNeg = firstNeg + numNeg
-        return indexPos, indexNeg
+        #print indexPos, indexNeg
+        self.listPos = indexPos
+        self.listNeg = indexNeg
+        return
 
-    def cross_val_score(self, model, X, Y, cv = 10):
-        X = self.normalize(X)
-        allData = np.c_[X, Y]
-        self.folds = cv
-        ratio = 1.0 / cv
-        listPos, listNeg = self.generateValTrainSets(allData, ratio)
+    def cross_val_score(self, model):
+
         scores = []
         for i in range(self.folds):
-
-            dataNegVal = self.dataNeg[listNeg[i][0]:listNeg[i][1], :]
-            dataNegTrain = np.append(self.dataNeg[:listNeg[i][0], :], self.dataNeg[listNeg[i][1]:, :], axis = 0)
-
-            dataPosVal = self.dataPos[listPos[i][0]:listPos[i][1], :]
-            dataPosTrain = np.append(self.dataPos[:listPos[i][0], :], self.dataPos[listPos[i][1]:, :], axis = 0)
-
-            valSet = np.append(dataPosVal, dataNegVal, axis = 0)
-            trainSet = np.append(dataPosTrain, dataNegTrain, axis = 0)
-
-            valSet = self.shuffle(valSet)
-            trainSet = self.shuffle(trainSet)
-            
-            Xtrain = trainSet[:, :-1]
-            Ytrain = trainSet[:, -1]
-            Xtest = valSet[:, :-1]
-            Ytest = valSet[:, -1] 
+            Xtrain, Ytrain, Xtest, Ytest = self.train_test_set(i)
             model.fit(Xtrain, Ytrain)
             Ypred = model.predict(Xtest)
             accuracy = accuracy_score(Ytest, Ypred)
             scores.append(accuracy)
         return np.array(scores)
+
+
+    def train_test_set(self, i):
+        listPos = self.listPos
+        listNeg = self.listNeg
+        dataNegVal = self.dataNeg[listNeg[i][0]:listNeg[i][1], :]
+        dataNegTrain = np.append(self.dataNeg[:listNeg[i][0], :], self.dataNeg[listNeg[i][1]:, :], axis = 0)
+
+        dataPosVal = self.dataPos[listPos[i][0]:listPos[i][1], :]
+        dataPosTrain = np.append(self.dataPos[:listPos[i][0], :], self.dataPos[listPos[i][1]:, :], axis = 0)
+
+        valSet = np.append(dataPosVal, dataNegVal, axis = 0)
+        trainSet = np.append(dataPosTrain, dataNegTrain, axis = 0)
+
+        valSet = self.shuffle(valSet)
+        trainSet = self.shuffle(trainSet)
+
+        Xtrain = trainSet[:, :-1]
+        Ytrain = trainSet[:, -1]
+        Xtest = valSet[:, :-1]
+        Ytest = valSet[:, -1] 
+        return Xtrain, Ytrain, Xtest, Ytest
+
+
+
 
 
 
--- testCrossVal.py
+++ testCrossVal.py
@@ -1,7 +1,12 @@
 import numpy as np
 import sys
 from CrossValidation import CrossValidation
+from sklearn import svm
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 if len(sys.argv) > 1:
     filename = 'data/' + sys.argv[1]
@@ -13,7 +18,42 @@ print sys.argv[1]
 allData = np.loadtxt(filename, delimiter=',')
 Xtrain = allData[:, :-1]
 Ytrain = allData[:, -1] 
+n_features = Xtrain.shape[1]
 myModel = linear_model.LogisticRegression()
-scores = CrossValidation().cross_val_score(myModel, Xtrain, Ytrain, cv=10)
-print scores
-print scores.mean()
+clf = RandomForestClassifier(n_estimators=10)
+myModel = svm.SVC(kernel = 'linear', C = 1)
+
+# clf = RandomForestClassifier(n_estimators=50, max_depth=None, min_samples_split=1, random_state=0, max_features=int(pow(n_features, 0.5)))
+
+# scores = CrossValidation().cross_val_score(clf, Xtrain, Ytrain, cv=10)
+# #print scores
+# print "RandomForestClassifier : ", scores.mean()
+
+# clf = ExtraTreesClassifier(n_estimators=120, max_depth=None, min_samples_split=1, random_state=0, max_features=int(pow(n_features, 0.5)))
+
+# scores = CrossValidation().cross_val_score(clf, Xtrain, Ytrain, cv=10)
+# print "ExtraTreesClassifier : ", scores.mean()
+
+# clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), n_estimators=400, algorithm="SAMME.R")
+
+# scores = CrossValidation().cross_val_score(clf, Xtrain, Ytrain, cv=10)
+# print "AdaBoostClassifier : ", scores.mean()
+
+clf = linear_model.LogisticRegression()
+
+scores = CrossValidation(Xtrain, Ytrain, cv=10).cross_val_score(clf)
+print "LogisticRegression : ", scores.mean()
+
+# M witclf = svm.SVC(kernel = 'linear', C = 0.01)
+
+# scores = CrossValidation().cross_val_score(clf, Xtrain, Ytrain, cv=10)
+# print "SVM with linear kernel (C = 0.01): ", scores.mean()
+
+# clf = svm.SVC(kernel='rbf', gamma = 1, C = 50)
+
+# scores = CrossValidation().cross_val_score(clf, Xtrain, Ytrain, cv=10)
+# print "SVh gaussian kernel (C = 50, gamma = 1): ", scores.mean()
+
+print ""
+
+
